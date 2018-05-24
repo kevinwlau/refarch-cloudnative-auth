ac5dbddaaa3621c8d3e6ff15d6858a6956543a3f
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2018-05-24 15:43:18 -0500

    Removing unused imports

--- src/main/java/application/rest/HealthEndpoint.java
+++ src/main/java/application/rest/HealthEndpoint.java
@@ -1,12 +1,6 @@
 package application.rest;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
