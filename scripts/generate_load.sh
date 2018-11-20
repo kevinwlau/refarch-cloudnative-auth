411fb943d97e156a4febdc6b249359b87bf4a4d2
Fabio Gomez <fabiogomez@us.ibm.com>
2018-11-19 23:24:52 -0600

    - Simplified generate_load.sh script

--- scripts/generate_load.sh
+++ scripts/generate_load.sh
@@ -1,37 +1,21 @@
 #!/bin/bash
-HOST="localhost";
-PORT="8083";
-URL="http://${HOST}:${PORT}";
-DEPLOYMENT="auth-auth";
+URL="$1";
 SERVICE_PATH="oauth/token";
 
 TEST_USER=user;
 TEST_PASSWORD=passw0rd;
 
-# trap ctrl-c and call ctrl_c() to stop port forwarding
-trap ctrl_c INT
-
-function ctrl_c() {
-	echo "** Trapped CTRL-C... Killing Port Forwarding and Stopping Load";
-	killall kubectl;
-	exit 0;
-}
-
-function start_port_forwarding() {
-	echo "Forwarding service port ${PORT}";
-	kubectl port-forward deployment/${DEPLOYMENT} ${PORT}:${PORT} --pod-running-timeout=1h &
-	echo "Sleeping for 3 seconds while connection is established...";
-	sleep 3;
-}
-
-# Port Forwarding
-start_port_forwarding
+if [ -z "$URL" ]; then
+	URL="http://localhost:8083"
+	echo "No URL provided! Using ${URL}"
+fi
 
 # Load Generation
-echo "Generating load..."
+echo "Generating load on ${URL}/${SERVICE_PATH}"
 
 while true; do
 	curl -s -X POST -u bluecomputeweb:bluecomputewebs3cret \
 		${URL}/${SERVICE_PATH}\?grant_type\=password\&username\=${TEST_USER}\&password\=${TEST_PASSWORD}\&scope\=blue > /dev/null;
+	echo -n .;
 	sleep 0.2;
 done
